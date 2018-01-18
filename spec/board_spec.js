bfb9db382b67ae9b116a96b6a77bc9e53fde240d
Betty <b.lam4392@gmail.com>
2018-01-18 15:41:16 -0800

    Specs for Board initialize

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -1,5 +1,59 @@
-describe("board", () => {
-  it("can test", () => {
-    expect(test("testing")).toEqual("It works");
+describe('A Board', () => {
+  const board = new Board();
+
+  describe('initializes', () => {
+    const smallBoard = new Board(3);
+
+    it('with an empty 10x10 grid by default', () => {
+      const grid = board.grid;
+      expect(grid.length).toEqual(10);
+      grid.forEach( row =>  {
+        expect(row.length).toEqual(10);
+        expect(row.every(pos => pos === undefined)).toBe(true);
+      });
+    });
+
+    it('with an empty NxN grid if given N', () => {
+      const grid = smallBoard.grid;
+      expect(grid.length).toEqual(3);
+      grid.forEach( row =>  {
+        expect(row.length).toEqual(3);
+        expect(row.every(pos => pos === undefined)).toBe(true);
+      });
+    });
+
+    it('with zero active ships', () => {
+      expect(board.activeShips).toEqual(0);
+      expect(smallBoard.activeShips).toEqual(0);
+    });
   });
+
+  describe('when setting up', () => {
+    it('places ships at specified positions on the grid', () => {
+
+    });
+
+    it('tracks the number of active ships', () => {
+
+    });
+  });
+
+  describe('when in play', () => {
+    it('checks if a position has been attacked', () => {
+
+    });
+
+    it('will update the grid if the attack missed', () => {
+
+    });
+
+    it('will update the grid if a ship was hit', () => {
+
+    });
+
+    it('will update the number of active ships if a ship was sunk', () => {
+
+    });
+  });
+
 });
