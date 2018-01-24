9740d629adacbe207069994ad53052b92ed0f5f1
Betty <b.lam4392@gmail.com>
2018-01-23 20:00:12 -0800

    Start game of battleship on 5x5 grid and 2 ships

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 require('babel-register');
 import Battleship from './battleship.js';
-import Board from './board.js';
 
-let b = new Battleship();
-// b.board1.display(false);
-// let ships = b.setup(b.board1, [...b.pieces]);
-b.play();
-// let s = new Board();
-// console.log(s.checkPos([0,0]));
-// console.log(s.checkPos([0,9]));
-// console.log(s.checkPos([9,9]));
-// console.log(s.checkPos([10,10]));
+let game = new Battleship(5, [2, 2]);
+game.play();
